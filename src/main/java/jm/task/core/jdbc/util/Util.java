f4d972a4e4e148836c3f86d5cd07604da2e675ab
tupercav <tupercav@gmail.com>
2022-06-29 12:09:04 +0300

    Made some fields private

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    static String url = "jdbc:mysql://localhost:3306/mydbtest";
-    static String username = "root";
-    static String password = "root";
+    private static String url = "jdbc:mysql://localhost:3306/mydbtest";
+    private static String username = "root";
+    private static String password = "root";
     public static Connection getConnection() {
         Connection connection;
         try {
